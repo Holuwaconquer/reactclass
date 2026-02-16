6207d57e3ee2f7f5c35b07ace70a7e27853c3982
A-Samad-Dev <olokoadesola1@gmail.com>
2026-02-16 11:06:08 +0100

    created a new branch

--- src/pages/PostdetailsPage.jsx
+++ src/pages/PostdetailsPage.jsx
@@ -1,9 +1,36 @@
-import React from 'react'
+import axios from "axios";
+import React, { useState } from "react";
+import { useLocation, useParams } from "react-router";
 
 const PostdetailsPage = () => {
-  return (
-    <div>PostdetailsPage</div>
-  )
-}
+  const { id } = useParams();
+  const location = useLocation();
+  const apiUrl = `https://jsonplaceholder.typicode.com/posts/${id}`;
+  const [datav, setdatav] = useState(null);
+  axios
+    .get(apiUrl)
+    .then((response) => {
+      setdatav(response.data);
+    })
+    .catch((err) => {
+      console.error("There is an error while fetching the data", err);
+    })
+    .finally(() => {});
+    
+    const data = location.state?.data || datav;
+    console.log(data);
 
-export default PostdetailsPage
+  if (!data) {
+    return <p>loading...</p>;
+  }
+
+  return (
+    <div className="px-9! flex flex-col justify-evenly box-border w-3/4 border h-80 mx-auto! rounded-md bg-red-200 ">
+      <h1>userId:{data?.userId}</h1>
+      <p>Title: {data?.title.split(" ").splice(0, 10).join(" ")} </p>
+      <p>body: {data?.body.split(" ").splice(0, 10).join(" ")} </p>
+    </div>
+  );
+};
+
+export default PostdetailsPage;
